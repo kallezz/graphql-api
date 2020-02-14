9d6bcad4f1c755180d88ce386621c6914fbb04c1
Kalle Melender <kalle.melender@elido.fi>
2020-02-14 16:59:09 +0200

    #21.09 Authentication

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,6 +1,9 @@
 // Hashing
 const bcrypt = require('bcrypt');
 
+// Token
+const jwt = require('jsonwebtoken');
+
 // Models
 const User = require('../../models/User');
 
@@ -20,6 +23,25 @@ module.exports = {
             return {...result._doc, password: null}
         } catch (e) {
             throw e;
+        }
+    },
+    login: async ({ email, password }) => {
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error('User does not exist!');
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            throw new Error('Password is incorrect!');
+        }
+        // TODO: Secure secret for JWT
+        const token = jwt.sign({userId: user._id, email: user.email}, 'somesecretforlocal', {
+            expiresIn: '24h'
+        });
+        return {
+            userId: user._id,
+            token,
+            tokenExpiration: 24
         }
     }
 };
